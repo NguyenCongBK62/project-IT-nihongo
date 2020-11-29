aee6f8beafcff3fada27f04499f817bc4f5c94f1
Ubuntu <ubuntu@ip-172-31-3-164.ec2.internal>
2020-11-29 03:11:20 +0000

    add name to article

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :articles
   devise_for :users
+  get 'users/:id' => 'users#show', as: :user
+  get "users" , to: "users#index"
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
